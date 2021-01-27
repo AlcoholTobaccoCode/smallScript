05e5e5dca51ca5e542c9c18d57ddc43c9a55b640
unknown <duqings@foxmail.com>
2021-01-28 01:51:52 +0800

    end

---
+++ assets/js/animeSection.js
@@ -0,0 +1,110 @@
+function animePart_0() {
+
+  /* //! S 第一部分
+    ****************************************/
+  animeGather.section['section_part_0'] = {};
+  //* 第一部分主体
+  animeGather.section['section_part_0']['Body'] = {
+    targets: 'section.part_0',
+    opacity: 1,
+  };
+
+  //* 第一部分 - 选择
+  //* 入
+  animeGather.section['section_part_0']['icon_choose_in'] = {
+    targets: '.icon-choose-body',
+    left: 0,
+    opacity: 1,
+  };
+  //* 出
+  animeGather.section['section_part_0']['icon_choose_out'] = {
+    targets: '.icon-choose-body',
+    left: -globalClient.x,
+    opacity: 0,
+    delay: 200,
+    duration: 2000
+  };
+
+  //* 第一部分 - 第一段欢迎词
+  //* 入
+  animeGather.section['section_part_0']['welcome_0_in'] = {
+    targets: 'section.part_0 .welcome_0',
+    // easing: 'easeInOutQuad',
+    opacity: 1,
+  };
+  //* 出
+  animeGather.section['section_part_0']['welcome_0_out'] = {
+    targets: 'section.part_0 .welcome_0',
+    // easing: 'easeInOutQuad',
+    opacity: 0,
+  };
+
+  //* 第一部分 - 第二段欢迎词
+  //* 入
+  animeGather.section['section_part_0']['welcome_1_in'] = {
+    targets: 'section.part_0 .welcome_1',
+    // easing: 'easeInOutQuad',
+    opacity: 1,
+  };
+  //* 出
+  animeGather.section['section_part_0']['welcome_1_out'] = {
+    targets: 'section.part_0 .welcome_1',
+    // easing: 'easeInOutQuad',
+    opacity: 0,
+  };
+
+  //* 第一部分 - 渲染已选伴友
+  // animeGather.section['section_part_0']['follow_friend_part_0_in'] = ;
+
+
+  /* //! E 第一部分
+   ****************************************/
+  animeGather.section['section_part_0']['Body'] = {
+    targets: 'section.part_1',
+    opacity: 1,
+  };
+
+  /* //! S 第二部分
+   ****************************************/
+
+
+
+  /* //! E 第二部分
+   ****************************************/
+
+  //* 更新时间线
+  const animePlayQueue = {
+    chooseBefore() { // *选择前
+      //? 第一部分
+      animeTimeline
+        .add(animeGather.section['section_part_0']['Body']) //* 身体显
+        .add(animeGather.section['section_part_0']['icon_choose_in'], '+=500');
+      setTimeout(() => {
+        animeTimeline.pause();
+      }, 5000);
+
+      animeTimeline
+        .add(animeGather.section['section_part_0']['icon_choose_out'], '+=1000')
+        .add({
+          targets: '.follow-friend',
+          // easing: 'easeInOutQuad',
+          bottom: '1rem',
+          left: '5rem',
+          opacity: 1,
+        }) //* 已选伴友 入
+        .add(animeGather.section['section_part_0']['welcome_0_in']) //* 欢迎词 0 入
+        .add(animeGather.section['section_part_0']['welcome_0_out'], '+=2500') //* 欢迎词 0 出
+        .add({
+          targets: '.follow-friend',
+          // easing: 'easeInOutQuad',
+          bottom: '1rem',
+          left: '4rem',
+          opacity: 1,
+        }) //* 已选伴友 入
+        .add(animeGather.section['section_part_0']['welcome_1_in']) //* 欢迎词 1 入
+        .add(animeGather.section['section_part_0']['welcome_1_out']) //* 欢迎词 1 出
+    }
+  };
+
+  return animePlayQueue;
+}
---
+++ assets/js/index.js
@@ -0,0 +1,185 @@
+/*
+ * @description: index 页面支持
+ * @Author: duQings duqings@foxmail.com
+ * @Date: 2021年1月27日19:39:24
+ * @FilePath: assets/js/index.js
+ */
+
+var globalClient = {}, //* 全局窗口尺寸存储
+  animeGather = {}, //* 动画对象集合
+  animeTimeline = anime.timeline({
+    easing: 'easeInOutQuad',
+    duration: 2000
+  }), //* 创建动画时间轴
+  jumpRabbitKeyframes = {
+    translateY: [{
+        value: 10,
+        duration: 50,
+        delay: 100
+      },
+      {
+        value: -50,
+        duration: 150,
+        delay: 100
+      },
+      {
+        value: 0,
+        duration: 100,
+        delay: 100
+      },
+      {
+        value: 0,
+        duration: 100,
+        delay: 1200
+      }
+    ]
+  },
+  varTem; //* 临时占位变量
+
+$(function () {
+
+  //* 初始化公共变量
+  initCommonVariate();
+
+  //* 初始化 anime 动画对象
+  initAnime();
+
+  //* 开始播放
+  animePlay();
+
+  //* 渲染选择动物
+  loadIconChooser();
+
+  //* 加载页面点击事件
+  pageEventsInAnime();
+});
+
+/**
+ * 初始化公共变量
+ */
+function initCommonVariate() {
+
+  globalClient['x'] = $('body').width();
+  globalClient['y'] = $('body').height();
+
+  //* 各部分盒子高度始终等于窗口高度
+  $('section').css({
+    'height': globalClient.y,
+    // 'left': -globalClient.x
+  });
+
+  $('.icon-choose-body').css({
+    'top': (globalClient.x / 4),
+    'width': globalClient.x,
+    'opacity': 0,
+  });
+
+  $('.follow-friend').css({
+    'left': -globalClient.x,
+    'opacity': 0,
+  });
+
+}
+
+/**
+ * 初始化 anime 动画对象
+ */
+function initAnime() {
+
+  //* 创建时间轴
+  /* animeTimeline = anime.timeline({
+    easing: 'easeInOutQuad',
+    duration: 2000
+  }); */
+
+  animeGather['section'] = {}; //* 创建主体存储变量
+
+  /*//? S part_0
+   ****************************************/
+  // animePart_0();
+  /*//? S part_1
+   ****************************************/
+}
+
+/**
+ * 动画播放顺序控制
+ * @param {string} part_0 部分、节点
+ * @param {string} period 周期
+ */
+function animePlay(part, period) {
+  if (!part || !period) {
+    animeTimelineControl().part_0.chooseBefore(); //* 进入系统默认播放第一部分前奏
+  } else {
+    animeTimelineControl()[part][period]();
+  }
+  
+
+}
+
+/**
+ * 时间线播放
+ */
+function animeTimelineControl() {
+  const timeLine = {
+    part_0: animePart_0(),
+  }
+
+  return timeLine;
+}
+
+
+/**
+ * 加载页面点击事件
+ */
+function pageEventsInAnime() {
+
+  //* 点击水波纹效果
+  $(document).on('click', '.ani-ripple', function (e) {
+    var target = $(this)[0];
+    $(target).css({
+      'overflow': 'hidden',
+      'position': 'relative'
+    });
+    var rect = target.getBoundingClientRect();
+    var ripple = target.querySelector('.ripple');
+    if (!ripple) {
+      ripple =
+        document.createElement('span');
+      ripple.className = 'ripple'
+      ripple.style.height = ripple.style.width = Math.max(rect.width,
+        rect.height) + 'px'
+      target.appendChild(ripple);
+    }
+    ripple.classList.remove('show');
+    var top = e.pageY - rect.top -
+      ripple.offsetHeight / 2 - document.body.scrollTop;
+    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    ripple.style.top = top + 'px'
+    ripple.style.left = left + 'px'
+    ripple.classList.add('show');
+    return false;
+  });
+
+  //*选择伴友
+  $(document).on('click', '.icon-choose-btn', function() {
+    //* 存储伴友
+    $('.follow-friend').html($(this).parents('.icon-block').prop('outerHTML'));
+    //* 开始播放动画
+    animeTimeline.play();
+  });
+}
+
+/**
+ * 窗口尺寸改变时
+ * 
+ * windowResizeTimerInAnime 防抖变量
+ */
+let windowResizeTimerInAnime = null;
+$(window).resize(function () {
+  if (windowResizeTimerInAnime) {
+    clearTimeout(windowResizeTimerInAnime);
+  }
+  windowResizeTimerInAnime = setTimeout(function () {
+    initCommonVariate();
+  }, 100);
+});
